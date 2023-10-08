dc4a3ddf6c2a4329036b41c3d103ab39306ca3cf
raynyator <rayn.yator@student.moringaschool.com>
2023-10-08 12:23:36 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    maketodo(e.target.tasks.value)
+    
+  })
 });
+function maketodo(todo){
+  let p=document.createElement('p')
+  p.textContent=`${todo}`
+  let btn=document.createElement('button')
+  btn.textContent='x'
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(pz)
+}
